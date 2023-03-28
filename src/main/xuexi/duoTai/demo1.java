8190e5f2989ea8e06788ebaff9e9756d1c79fdc8
LMR <3120639307@qq.com>
2023-03-28 21:29:42 +0800

    多态

---
+++ src/main/xuexi/duoTai/demo1.java
@@ -0,0 +1,36 @@
+package duoTai;
+class Test1{
+    public  void test1() {
+        System.out.println("Test1____1");
+    }
+    public void test2() {
+        System.out.println("Test1____2");
+    }
+}
+class Test2 extends Test1{
+    @Override
+    public void test1() { //重写Test1中的test1方法
+        System.out.println("Test2____1");
+    }
+    public void test3() {
+        System.out.println("Test2____2");
+    }
+}
+public class demo1 {
+    public static void main(String[] args) {
+        //向上转型
+        //Test1 test1 = new Test2();
+//         Test2 test2 = new Test2();
+//         Test1 test1 = test2;
+//         test1.test1();
+//         test1.test2();
+//         test2.test3();
+        Test1 test1 = new Test2();
+        Test2 test2 = (Test2) test1;
+        test2.test1();
+        test2.test2();
+        test2.test3();
+
+    }
+
+}
