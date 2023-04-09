a3bc5ad8ae38778b2c7aa3f3b7efd39cfffd56d8
LMR <3120639307@qq.com>
2023-04-09 11:25:34 +0800

    封装类

---
+++ src/main/xuexi/changyonglei/fengzhuanglei.java
@@ -0,0 +1,28 @@
+package changyonglei;
+
+public class fengzhuanglei {
+    public static void main(String[] args) {
+
+//        作用一: 字符串转换为基本数据类型
+//        作用二: 集合
+//        基本类型 类类型
+//        byte      java.lang.Byte
+//        short     java.lang.Short
+//        int       java.lang.Integerlong
+//        long      java.lang.Long
+//        float     java.lang.Float
+//        double    java.lang.Double
+//        boolean   java.lang.Boolean
+//        char      java.lang.Character
+
+        //int转String
+        int a = 2;
+        String b = String.valueOf(a);
+        System.out.println(b);
+        //String 转int
+        String c = "3";
+        int d = Integer.parseInt(c);
+        System.out.println(d);
+
+    }
+}
