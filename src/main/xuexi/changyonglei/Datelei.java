231103852c82cb5c43136b3dc977a674251bbf99
LMR <3120639307@qq.com>
2023-04-09 11:10:49 +0800

    日期类

--- src/main/xuexi/changyonglei/Datelei.java
+++ src/main/xuexi/changyonglei/Datelei.java
@@ -1,12 +1,11 @@
 package changyonglei;
 
-import java.text.ParseException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class Datelei {
     public static void main(String[] args) throws Exception{
-        //import java.util.Date;日期类
+        //import java.util.Date;日期类 通常用于日期类的计算 Date中华存储的是距离一个历史节点(1970年)的毫秒数
         //import java.text.SimpleDateFormat; 日期转换类
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -17,6 +16,12 @@ public class Datelei {
         Date idate = simpleDateFormat.parse(sdate);
         System.out.println(idate);
 
+        //计算日期
+        long time = date.getTime();
+        long date2 =(time+(12L*24*60*60*1000));
+        System.out.println("今天是"+simpleDateFormat.format(time));
+        String formatdate = simpleDateFormat.format(date2);
+        System.out.println(formatdate);
 
     }
 }
